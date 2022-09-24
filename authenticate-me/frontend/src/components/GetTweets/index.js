7537c6f8be83522fb1e65ae91da7f240f3933a89
Sally <sallykim13@gmail.com>
2022-09-24 16:27:14 -0500

    commit

--- authenticate-me/frontend/src/components/GetTweets/index.js
+++ authenticate-me/frontend/src/components/GetTweets/index.js
@@ -8,65 +8,65 @@ function GetTweets({ tweets }) {
     const history = useHistory();
 
     console.log(tweets);
-    if
+    // if
 
     return (
-            <>
-                {tweets && (
-                    Object.values(tweets).map((tweet, index) => {
-                        return (
-                            <div className='tweet-container' key={index} onClick={(() => {
-                                history.push(`/tweets/${tweet?.id}`)
-                            })}>
+        <>
+            {tweets && (
+                Object.values(tweets).map((tweet, index) => {
+                    return (
+                        <div className='tweet-container' key={index} onClick={(() => {
+                            history.push(`/tweets/${tweet?.id}`)
+                        })}>
 
-                                <div className='profile-img'>
-                                    <Link to={{
-                                        pathname: `/${tweet?.User?.username}`,
-                                        state: {
-                                            userPageId: tweet?.User?.id
-                                        }
-                                    }}>
+                            <div className='profile-img'>
+                                <Link to={{
+                                    pathname: `/${tweet?.User?.username}`,
+                                    state: {
+                                        userPageId: tweet?.User?.id
+                                    }
+                                }}>
 
-                                        <img className='profile-img' src={tweet?.User?.profileImage} />
+                                    <img className='profile-img' src={tweet?.User?.profileImage} />
 
-                                    </Link>
+                                </Link>
+                            </div>
+                            <div className='tweet-text-box'>
+                                <div>
+                                    <h5 onClick={(() => {
+                                        history.push(`/${tweet?.User?.username}`)
+                                    })}>{tweet?.User?.firstName}  </h5>
+                                    {/* <h5><span className='thin-styling'>@{tweet?.User?.username} • {tweet?.updatedAt[1]} {tweet?.updatedAt[2]}</span></h5> */}
                                 </div>
-                                <div className='tweet-text-box'>
-                                    <div>
-                                        <h5 onClick={(() => {
-                                            history.push(`/${tweet?.User?.username}`)
-                                        })}>{tweet?.User?.firstName}  </h5>
-                                        {/* <h5><span className='thin-styling'>@{tweet?.User?.username} • {tweet?.updatedAt[1]} {tweet?.updatedAt[2]}</span></h5> */}
-                                    </div>
 
-                                    <div>
-                                        <p>{tweet?.tweet}</p>
-                                    </div>
-
-
-                                    <div className='settings-btn' >
-                                        <TweetSettingsModal tweet={tweet} />
-                                    </div>
-
+                                <div>
+                                    <p>{tweet?.tweet}</p>
                                 </div>
-                                <div className='tweet-icons-box'>
-                                    <div>
-                                        <i className="fa-regular fa-comment"></i>{tweet?.commentCount}
-                                    </div>
-                                    <div>
-                                        <i className="fa-solid fa-retweet"></i>{tweet?.retweetCount}
-                                    </div>
-                                    <div>
-                                        <i className="fa-regular fa-heart"></i>{tweet?.commentCount}
-                                    </div>
+
+
+                                <div className='settings-btn' >
+                                    <TweetSettingsModal tweet={tweet} />
+                                </div>
+
+                            </div>
+                            <div className='tweet-icons-box'>
+                                <div>
+                                    <i className="fa-regular fa-comment"></i>{tweet?.commentCount}
+                                </div>
+                                <div>
+                                    <i className="fa-solid fa-retweet"></i>{tweet?.retweetCount}
+                                </div>
+                                <div>
+                                    <i className="fa-regular fa-heart"></i>{tweet?.commentCount}
                                 </div>
                             </div>
+                        </div>
 
-                        )
-                    })
-                )}
-            </>
-        )
+                    )
+                })
+            )}
+        </>
+    )
 }
 
 export default GetTweets;
