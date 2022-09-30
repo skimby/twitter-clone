3a9861f0901ea7d28b2e3635feb811ff4d102bbd
Sally <sallykim13@gmail.com>
2022-09-30 18:23:53 -0500

    e

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,13 +4,13 @@ const apiRouter = require("./api");
 //
 router.use("/api", apiRouter);
 //
-router.get("/api/csrf/restore", (req, res) => {
-  const csrfToken = req.csrfToken();
-  res.cookie("XSRF-TOKEN", csrfToken);
-  res.status(200).json({
-    "XSRF-Token": csrfToken
-  });
-});
+// router.get("/api/csrf/restore", (req, res) => {
+//   const csrfToken = req.csrfToken();
+//   res.cookie("XSRF-TOKEN", csrfToken);
+//   res.status(200).json({
+//     "XSRF-Token": csrfToken
+//   });
+// });
 
 // Static routes
 // Serve React build files in production
