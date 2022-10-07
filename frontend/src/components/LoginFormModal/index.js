675b5a7ee6da2dcc96a83df4ad3f7f89b72e0c69
Sally <sallykim13@gmail.com>
2022-10-06 19:33:27 -0500

    s

--- frontend/src/components/LoginFormModal/index.js
+++ frontend/src/components/LoginFormModal/index.js
@@ -8,7 +8,7 @@ function LoginFormModal() {
   return (
     <>
 
-      <button onClick={() => setShowModal(true)}>Log In</button>
+      <button className="login-btn" onClick={() => setShowModal(true)}>Log In</button>
       {showModal && (
         <Modal onClose={() => setShowModal(false)}>
           <LoginForm setShowModal={setShowModal} />
--- frontend/src/components/SignupPage/index.js
+++ frontend/src/components/SignupPage/index.js
@@ -1,14 +1,30 @@
 import LoginFormModal from "../LoginFormModal";
 import SignupFormModal from "../SignupFormModal";
+import coverImg from '../../images/cover-image.jpeg';
+import logo from '../../images/twitter-logo.png';
+
+import './SignupPage.css';
+
 
 function SignupPage() {
 
     return (
         <>
-            <h1>Happening now</h1>
-            <h2>Join Twitter today.</h2>
-            <LoginFormModal />
-            <SignupFormModal />
+            <div className="signup-page-container">
+                <div className="cover-img-container">
+                    <img src={coverImg} className="cover-img" />
+                </div>
+
+                <div className="cover-text-box">
+                    <img src={logo} className='logo-box-signup' />
+                    <h1 className="big-header">Happening now</h1>
+                    <h2 className='medium-header'>Join Twitter today.</h2>
+                    <div className='login-buttons'>
+                        <LoginFormModal />
+                        <SignupFormModal />
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
