e6154d0aaa18cf21c00aaa0009a1e96bc358325a
Sally <sallykim13@gmail.com>
2022-10-08 13:44:00 -0500

    s

--- backend/db/models/tweet.js
+++ backend/db/models/tweet.js
@@ -21,12 +21,7 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Tweet.init({
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true
-    },
+
     userId: {
       type: DataTypes.INTEGER,
       allowNull: false
